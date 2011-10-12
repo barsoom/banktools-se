be58d2bc903c8c64c01bd355f05afc5ed83bfb82
Henrik N <henrik@nyh.se>
2011-10-12 11:08:14 +0200

    Initial commit.

---
+++ lib/bank-tools-se.rb
@@ -0,0 +1,9 @@
+require "bank-tools-se/version"
+
+module Bank
+  module Tools
+    module Se
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/bank-tools-se/version.rb
@@ -0,0 +1,7 @@
+module Bank
+  module Tools
+    module Se
+      VERSION = "0.0.1"
+    end
+  end
+end
