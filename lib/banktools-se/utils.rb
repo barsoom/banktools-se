0b2a735d1bfb93cd268d626d5db7f97405ed4714
Henrik N <henrik@nyh.se>
2011-10-13 09:59:53 +0200

    Use each_with_index instead of enum_with_index which was removed from 1.9.

--- lib/banktools-se/utils.rb
+++ lib/banktools-se/utils.rb
@@ -3,10 +3,9 @@ module BankTools
     module Utils
 
       # Based on http://blog.internautdesign.com/2007/4/18/ruby-luhn-check-aka-mod-10-formula
-      require "enumerator"
       def self.valid_luhn?(number)
         digits = number.to_s.scan(/\d/).reverse.map { |x| x.to_i }
-        digits = digits.enum_with_index.map { |d, i|
+        digits = digits.each_with_index.map { |d, i|
           d *= 2 if i.odd?
           d > 9 ? d - 9 : d
         }
