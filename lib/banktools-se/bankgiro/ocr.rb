591371b7695dbd9f089640942f2a223851d9b5e6
Henrik Nyh <henrik@nyh.se>
2014-01-17 22:05:02 +0100

    Cleanup.

--- lib/banktools-se/bankgiro/ocr.rb
+++ lib/banktools-se/bankgiro/ocr.rb
@@ -13,30 +13,32 @@ module BankTools
 
           number = number.to_s
 
-          if pad
-            number += pad
+          number += pad if pad
+
+          # Adding 2: 1 length digit, 1 check digit.
+          number += ((number.length + 2) % 10).to_s if add_length_digit
+
+          number_with_ocr = number + Utils.luhn_checksum(number).to_s
+
+          length = number_with_ocr.length
+          if length > 25
+            raise OverlongOCR, "Bankgiro OCR must be 2-25 characters (this one would be #{length} characters)"
           end
 
-          if add_length_digit
-            # Adding 2: 1 length digit, 1 check digit
-            number += ((number.length + 2) % 10).to_s
-          end
-
-          number_with_ocr = number.to_s + Utils.luhn_checksum(number).to_s
-          raise OverlongOCR, "Bankgiro OCR must be 2-25 characters" if number_with_ocr.length > 25
           number_with_ocr
         end
 
         def self.number_from_ocr(number, opts = {})
           strip_length_digit = opts.fetch(:length_digit, false)
-          strip_padding = opts.fetch(:pad, false)
+          strip_padding = opts.fetch(:pad, "")
 
           raise BadCheckDigit unless Utils.valid_luhn?(number)
 
-          number = number.to_s
-          number.slice!(-2) if strip_length_digit
-          number.slice!(-strip_padding.length.succ..-2) if strip_padding
-          number.chop
+          digits_to_chop  = 1  # Checksum.
+          digits_to_chop += 1 if strip_length_digit
+          digits_to_chop += strip_padding.length
+
+          number[0...-digits_to_chop]
         end
       end
     end
